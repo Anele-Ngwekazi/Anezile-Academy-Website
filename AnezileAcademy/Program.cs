e115fe2d331c4cc5dcc0cfba9001c7c03fd70957
Anele Ngwekazi <Anele-Ngwekazi@bscglobal.com>
2023-07-09 01:44:57 +0200

    upgrade from dotnet2.2 to net7

--- AnezileAcademy/Program.cs
+++ AnezileAcademy/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿
+
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace AnezileAcademy
 {
@@ -7,12 +9,15 @@ namespace AnezileAcademy
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateDefaultBuilder(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+        public static IHost CreateDefaultBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
                 .Build();
     }
 }
--- AnezileAcademy/Startup.cs
+++ AnezileAcademy/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using AnezileAcademy.Data;
 using AnezileAcademy.Models;
 using AnezileAcademy.Services;
+using Microsoft.Extensions.Hosting;
 
 namespace AnezileAcademy
 {
@@ -48,30 +45,37 @@ namespace AnezileAcademy
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, 
             ApplicationDbContext context, UserManager<ApplicationUser> userManager, 
             RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
+                app.UseHttpsRedirection();
+
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             app.UseStaticFiles();
 
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(routes =>
             {
-                routes.MapRoute(
+                routes.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
             SeedData.Initialize(context, userManager, roleManager).Wait();
